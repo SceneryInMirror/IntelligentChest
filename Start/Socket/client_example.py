d52969f5c6e1d3aea337d046488bbb5b2f74d94e
ykx <ykx150905@pku.edu.cn>
2018-05-04 23:58:50 +0800

    2018.05.04

--- Start/Socket/client_example.py
+++ Start/Socket/client_example.py
@@ -3,7 +3,7 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('162.105.91.29',9999))
 
-print (s.recv(1024).encode('utf-8'))
+print (s.recv(1024).decode('utf-8'))
 
 # s.send('GET / HTTPS/1.1\r\nHost: sceneryinmirror.github.io\r\nConnection: close\r\n\r\n')
 for data in [b'Michael', b'Tracy', b'Sarah']:
