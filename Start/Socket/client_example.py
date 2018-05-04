c57dd1e78b7e01dbd998ada7eda5c3cd711948d7
ykx@ESDC <ykx150905@gmail.com>
2018-05-04 23:54:31 +0800

    2018.05.04

--- Start/Socket/client_example.py
+++ Start/Socket/client_example.py
@@ -3,13 +3,13 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('162.105.91.29',9999))
 
-print s.recv(1024)
+print (s.recv(1024).encode('utf-8'))
 
 # s.send('GET / HTTPS/1.1\r\nHost: sceneryinmirror.github.io\r\nConnection: close\r\n\r\n')
-for data in ['Michael', 'Tracy', 'Sarah']:
+for data in [b'Michael', b'Tracy', b'Sarah']:
     s.send(data)
-    print s.recv(1024)
-s.send('exit')
+    print (s.recv(1024).decode('utf-8'))
+s.send(b'exit')
 s.close()
 
 # header,html = data.split('\r\n\r\n', 1)
