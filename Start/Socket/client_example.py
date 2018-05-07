141007b9e765d145cb19adc96a92083043c3d24b
ykx@ESDC <ykx150905@gmail.com>
2018-05-07 23:55:42 +0800

    2018.05.07

--- Start/Socket/client_example.py
+++ Start/Socket/client_example.py
@@ -1,19 +1,29 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('162.105.91.29',9999))
 
+#--------------------
+#remote connect part
+#--------------------
+#s.connect(('162.105.91.29',9999))
+
+#----------------
+#local test part
+#----------------
+s.connect(('127.0.0.1',9999))
+print ((s.recv(1024)).decode('utf-8'))
+
+#---------------
+# transmit data
+#---------------
+s.send(('upload\r\ntest.tar.gz').encode('utf-8'))
 print (s.recv(1024).decode('utf-8'))
 
-# s.send('GET / HTTPS/1.1\r\nHost: sceneryinmirror.github.io\r\nConnection: close\r\n\r\n')
+#-----------
+# test part
+#-----------
 for data in [b'Michael', b'Tracy', b'Sarah']:
     s.send(data)
     print (s.recv(1024).decode('utf-8'))
 s.send(b'exit')
 s.close()
-
-# header,html = data.split('\r\n\r\n', 1)
-
-# print header
-# with open('baidu.html','wb') as f:
-#    f.write(html)
